e11529437b620a2f1f34a1d3fb7a7ad5cf3789d8
Ayush Singh Kushwah <96888236+Ayroid@users.noreply.github.com>
2022-01-21 16:44:15 +0530

    Update L21_For_Loops.py

--- L21_For_Loops.py
+++ L21_For_Loops.py
@@ -3,7 +3,7 @@
 for i in "Python":                          # A normal for loop for travesing a string
     print(i)
 
-for j in ["Ayush","Asha","Goku","Vegeta"]:  # for loop to print elements of a list
+for j in ["Ayush","Gohan","Goku","Vegeta"]:  # for loop to print elements of a list
     print(j)
 
 for k in range(1,10,2):                     # range function is used to give elements between the lower & upper limit with n as steps to be jumped
