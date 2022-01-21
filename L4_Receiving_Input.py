ee9cb1832df139c691ba31099925533c9bbb0e62
Ayush Singh Kushwah <96888236+Ayroid@users.noreply.github.com>
2022-01-21 16:51:21 +0530

    Delete L4_Receiving_Input.py

--- L4_Receiving_Input.py
+++
@@ -1,3 +0,0 @@
-name=input("What is your name? ")                       # Inputting Name  (String)
-color=input("What is your favourite color? ")           # Inputting Color (String)
-print(name +" likes "+color)                            # Printing Output
