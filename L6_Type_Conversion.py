044a0f9518011cf200bb7a84a0822a26438bc019
Ayush Singh Kushwah <96888236+Ayroid@users.noreply.github.com>
2022-01-21 16:50:08 +0530

    Delete L6_Type_Conversion.py

--- L6_Type_Conversion.py
+++
@@ -1,3 +0,0 @@
-pounds=input("Enter your weight in pounds ")            # Inputting Weight(It gets stored as a String)
-kilos=0.454*int(pounds)                                 # Converting pounds into Kg by converting year(string) to int
-print("Your weight in Kilograms is ",kilos)             # Printing Weight
